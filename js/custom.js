39f66095b05fb312f945157d0d0461dc28bae80f
eldersquid <pristinpinky@gmail.com>
2021-07-09 23:19:54 +0800

    testuasruad

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+// -----------------------------------------------------
+	// -----------   TESTIMONIAL HOVER    ------------------
+	// -----------------------------------------------------
+
+	function waxon_tm_testimonials_effect(){
+
+		var list	= jQuery('.waxon_tm_testimonials .testi_inner .right .image_list ul li');
+
+		list.on('mouseenter',function(){
+			var element = jQuery(this);
+			var elIndex = element.index()+1;
+			list.removeClass('active');
+			element.addClass('active');
+			element.closest('.waxon_tm_testimonials').find('.quote_list ul li').removeClass('active');
+			element.closest('.waxon_tm_testimonials').find('.quote_list ul li:nth-child('+elIndex+')').addClass('active');
+		});
+	}
+	waxon_tm_testimonials_effect();
